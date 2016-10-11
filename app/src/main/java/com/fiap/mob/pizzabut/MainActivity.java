bcfa8bfd1b362fcdb0367155367afff9e3e4fbb1
Caue Bruno de Almeida <RM30917@fiap.com.br>
2016-10-10 23:06:26 -0300

    Implemeting funcs

--- app/src/main/java/com/fiap/mob/pizzabut/MainActivity.java
+++ app/src/main/java/com/fiap/mob/pizzabut/MainActivity.java
@@ -2,12 +2,73 @@ package com.fiap.mob.pizzabut;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
+
+    // interface => say the rules => CONTRACT
+
+    /**
+     * 1. Interface
+     * 2. Set listener
+     * 3. Implement 1.
+     */
+
+    private RadioGroup rg;
+    private CheckBox chkBorda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rg = (RadioGroup) findViewById(R.id.radiopizzaGroup);
+        chkBorda = (CheckBox) findViewById(R.id.checkBordaRecheada);
+
+        // 2.
+        rg.setOnCheckedChangeListener(this); // this => this activity
+    }
+
+    /**
+     * Calculates total's pizza price
+     */
+
+    public void calc(View v) {
+        int selectedPizzaFlavour = rg.getCheckedRadioButtonId();
+        int total = 0;
+
+        if (selectedPizzaFlavour == R.id.btnMussarela) {
+            total = 10;
+        } else {
+            if (selectedPizzaFlavour == R.id.btnCalabresa) {
+                total = 15;
+            } else {
+                total = 20; // calabresa
+            }
+
+            if (chkBorda.isChecked()) {
+                total += 5;
+            }
+        }
+
+        Toast.makeText(this, "Total: R$" + total, Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+        // Log.i("I variable is " + i);
+
+        if (i == R.id.btnMussarela) {
+            chkBorda.setChecked(false);
+            chkBorda.setEnabled(false);
+        } else {
+            chkBorda.setEnabled(true);
+        }
+
     }
 }
